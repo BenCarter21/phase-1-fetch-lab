7ded6466e8876a470d5d40cc4d0241f0f5d5fbfe
Ben <bc@bencarterconsulting.com>
2024-07-30 16:45:06 -0400

    Assignment completed.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
   
+  // renderBooks(json)
+  //.then(res => console.log(res))
+  //return res
+  
+  //let data = document.createElement('p').main.res
 }
-
+  // To pass the tests, don't forget to return your fetch!
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -15,3 +22,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+  // for all fetch labs, make sure to add "return" ahead of the fetch for it to pass properly. 
